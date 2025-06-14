1325ca2e3ee93f2a4fe3f74c204e46d4d4fd0923
Andrey Belik <andvitbel@gmail.com>
2025-06-14 06:33:53 +0300

    trying vercel

--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "DATABASE_URL=${DATABASE_URL}"
-
-alembic upgrade head
-
-PORT=${PORT:-8000}
-
-exec gunicorn app.main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:${PORT}
